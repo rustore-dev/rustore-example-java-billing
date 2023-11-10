9a2b54db2c50ff3245c91ce83d56ef4b4f0e0074
edudin <evgeniydudin9045@gmail.com>
2023-11-10 10:29:26 +0300

    updated version of billing client

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        mavenLocal()
         maven {
             url = uri("https://artifactory-external.vkpartner.ru/artifactory/maven")
         }
